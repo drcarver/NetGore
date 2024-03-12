c514d4286f0225e9d9c3b45ff6c1cddc58ae9b9c
drcarver <drcarver@drcarver.com>
2024-03-12 11:01:27 -0600

    Initial database conversion is complete.
    I now need to convert the codegeneration of
    sql tables to entiy framework from the demo server.

--- NetGore/obj/Debug/net8.0/NetGore.AssemblyInfo.cs
+++ NetGore/obj/Debug/net8.0/NetGore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetGore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e24ac2436e2517d40466a7d9d38b03de097644be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07aaecd00503565574f33df71624dd9830d572f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetGore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetGore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
