c7073e8970819df157b090f04e76b2e17a5a0768
drcarver <drcarver@drcarver.com>
2024-03-13 04:40:29 -0600

    Cleanup build files and fix solution

--- NetGore/obj/Debug/net8.0/NetGore.AssemblyInfo.cs
+++ NetGore/obj/Debug/net8.0/NetGore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetGore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07aaecd00503565574f33df71624dd9830d572f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93ab2cfe974a66392c97d70794f1895a920b4808")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetGore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetGore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
